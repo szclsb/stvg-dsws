2de5560b238a8d0a3977bcab98074d6efd2729ee
Claudio <szclsb@gmail.com>
2022-07-06 19:07:58 +0200

    impl jwt auth

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,23 @@
 import {init as initAuth} from './routes/auth-route'
 import {init as initAccount} from './routes/account-route'
 
-import {Application} from "express";
-import express from "express";
+import express, {Application} from "express";
 import {Db} from "mongodb";
 import {Config} from "./model/config";
+import {verifyJwt} from "./utils/auth-utils";
 
 export function init(config: Config, app: Application, db: Db): Application {
+    app.use(async (req, res, next) => {
+        const auth = req.header('Authorization');
+        if (auth && auth.startsWith('Bearer ')) {
+            try {
+                res.locals.account = await verifyJwt(auth.substring(7), config.secret);
+            } catch (e) {
+                console.warn(e)
+            }
+        }
+        next();
+    });
     app.use('/api/v1/auth', initAuth(config, express.Router(), db));
     app.use('/api/v1/account', initAccount(config, express.Router(), db));
     return app
--- backend/src/routes/account-route.ts
+++ backend/src/routes/account-route.ts
@@ -2,10 +2,20 @@ import {Router} from "express";
 import {ObjectID} from "bson";
 import {Db} from "mongodb";
 import {Config} from "../model/config";
+import {Account} from "../model/account";
 
 const collectionName = "accounts"
 
 export function init(config: Config, router: Router, db: Db): Router {
+    router.get("/self", (req, res) => {
+        const account: Account & {_id: ObjectID} = res.locals.account;
+        if (account) {
+            res.status(200).json(account);
+        }
+        else {
+            res.status(401).send();
+        }
+    });
     router.get("/:id", (req, res) => {
         const id = req.params.id as string;
         db.collection(collectionName).findOne({_id: new ObjectID(id)},
